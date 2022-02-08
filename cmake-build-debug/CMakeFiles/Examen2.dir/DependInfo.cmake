c62325b46fb28dd74f7c1c9deac50f5659617614
Eidrian <adrloboo3@gmail.com>
2022-02-08 11:47:49 -0600

    r

---
+++ cmake-build-debug/CMakeFiles/Examen2.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/adr20/Desktop/Codigos/Estructuras de Datos/Examen2/Archivos.cpp" "CMakeFiles/Examen2.dir/Archivos.cpp.obj" "gcc" "CMakeFiles/Examen2.dir/Archivos.cpp.obj.d"
+  "C:/Users/adr20/Desktop/Codigos/Estructuras de Datos/Examen2/Edge.cpp" "CMakeFiles/Examen2.dir/Edge.cpp.obj" "gcc" "CMakeFiles/Examen2.dir/Edge.cpp.obj.d"
+  "C:/Users/adr20/Desktop/Codigos/Estructuras de Datos/Examen2/main.cpp" "CMakeFiles/Examen2.dir/main.cpp.obj" "gcc" "CMakeFiles/Examen2.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Examen2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen2.dir/Archivos.cpp.obj"
+  "CMakeFiles/Examen2.dir/Archivos.cpp.obj.d"
+  "CMakeFiles/Examen2.dir/Edge.cpp.obj"
+  "CMakeFiles/Examen2.dir/Edge.cpp.obj.d"
+  "CMakeFiles/Examen2.dir/main.cpp.obj"
+  "CMakeFiles/Examen2.dir/main.cpp.obj.d"
+  "Examen2.exe"
+  "Examen2.exe.manifest"
+  "Examen2.pdb"
+  "libExamen2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Examen2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
