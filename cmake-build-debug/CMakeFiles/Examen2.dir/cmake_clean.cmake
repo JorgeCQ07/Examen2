dbf04b333da25b0fbc4587bb6ab4a569b9fb0720
Eidrian <adrloboo3@gmail.com>
2022-02-09 03:04:17 -0600

    Clase Menu

---
+++ cmake-build-debug/CMakeFiles/Examen2.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen2.dir/Archivos.cpp.obj"
+  "CMakeFiles/Examen2.dir/Archivos.cpp.obj.d"
+  "CMakeFiles/Examen2.dir/DisjointSets.cpp.obj"
+  "CMakeFiles/Examen2.dir/DisjointSets.cpp.obj.d"
+  "CMakeFiles/Examen2.dir/Edge.cpp.obj"
+  "CMakeFiles/Examen2.dir/Edge.cpp.obj.d"
+  "CMakeFiles/Examen2.dir/Graph.cpp.obj"
+  "CMakeFiles/Examen2.dir/Graph.cpp.obj.d"
+  "CMakeFiles/Examen2.dir/Menu.cpp.obj"
+  "CMakeFiles/Examen2.dir/Menu.cpp.obj.d"
+  "CMakeFiles/Examen2.dir/main.cpp.obj"
+  "CMakeFiles/Examen2.dir/main.cpp.obj.d"
+  "Examen2.exe"
+  "Examen2.exe.manifest"
+  "Examen2.pdb"
+  "libExamen2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Examen2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
